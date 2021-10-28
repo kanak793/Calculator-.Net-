725b19554670795787b7b8fd3a634bc0bcbf81fe
kanak793 <64707305+kanak793@users.noreply.github.com>
2021-10-28 15:44:29 +0530

    Update Program.cs

--- NetCoreCalculator/NetCoreCalculator/Program.cs
+++ NetCoreCalculator/NetCoreCalculator/Program.cs
@@ -19,7 +19,7 @@ namespace NetCoreCalculator
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+                
                 .Build();
     }
 }
